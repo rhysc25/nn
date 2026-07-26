a82a235e6de1ddb985c1f8b61ca04fb297c7da39
R.D. Cunningham <rdc50@cam.ac.uk>
2026-07-26 16:22:24 +0100

    Working multiplication, before major redesign (owner and view)

--- src/core/matrix.cpp
+++ src/core/matrix.cpp
@@ -27,9 +27,36 @@ void matrix::set_element(int r, int c, float val) {
     v[r * columns + c] = val;
 }
 
+int matrix::get_columns() {return columns;}
+void matrix::set_columns(int c) {columns = c;}
+int matrix::get_rows() {return rows;}
+void matrix::set_rows(int r) {rows = r;}
+
 void multiply(matrix& a, matrix& b) {
     a.list_contents();
     b.list_contents();
+
+    if (a.get_columns() != b.get_rows()) {
+        std::cout << "Error: Rows of A must equal columns of B."; 
+        return;
+    }
+
+    std::vector<float> cv(a.get_rows() * b.get_columns(), 0.0f);
+    matrix c(cv, a.get_rows(), b.get_columns());
+
+    c.list_contents();
+
+    for (int i = 0; i < c.get_rows(); i++) {
+        for (int j = 0; j < c.get_columns(); j++) {
+            float temp = 0;
+            for (int k = 0; k < a.get_columns(); k++) {
+                temp += (a.get_element(i, k) * b.get_element(k, j));
+            }
+            c.set_element(i, j, temp);
+        }
+    }
+
+    c.list_contents();
 }
 
 int main() {
--- src/core/matrix.h
+++ src/core/matrix.h
@@ -13,8 +13,13 @@ class matrix {
         matrix(std::vector<float>& vi, int ri, int ci);
         
         void list_contents();
+
         float get_element(int r, int c);
         void set_element(int r, int c, float val);
+        int get_columns();
+        void set_columns(int c);
+        int get_rows();
+        void set_rows(int r);
 };
 
 void multiply(matrix& a, matrix& b);
