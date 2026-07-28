8a9b48ff743f978a323efac2a77088165e694885
rhysc25 <rdc50@cam.ac.uk>
2026-07-28 22:26:04 +0100

    Fix

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ $OSTYPE=="msys" ]; then
+if [ $OSTYPE == "msys" ]; then
     cmake -B build
     cmake --build build
     ./build/Debug/nn.exe
 else 
     cmake -S . -B build -G "Unix makefiles"
     cmake --build build
-    .build/nn
+    ./build/nn
 fi
