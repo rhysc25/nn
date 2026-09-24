1221262385e0f29c93529afd18b08324440ef307
rhysc25 <rdc50@cam.ac.uk>
2026-09-24 21:55:56 +0100

    Sorted activation functions

--- src/model/mlp.cpp
+++ src/model/mlp.cpp
@@ -1,13 +1,20 @@
 #include "mlp.h"
 #include <iostream>
 
-layer::layer(size_t r, size_t c) : weights(r, c), bias(r, 1) {};
+Activation ReLU (
+    [] (float x) {return (x > 0.0f ? x : 0.0f);},
+    [] (float x) {return (x > 0.0f ? 1.0f : 0.0f);}
+);
+
+layer::layer(size_t r, size_t c, Activation act) : weights(r, c), bias(r, 1), activation(act) {};
+//layer::forward(MatrixView& x) {};
 
 int main() {
 
-    layer alpha = layer(3, 3);
+    layer alpha = layer(3, 3, ReLU);
 
     alpha.loadWeights({3.0f, 4.0f, 2.0f, 1.0f, 8.0f, 5.0f, 3.0f, 4.0f, 2.0f});
+    alpha.loadBias({3.0f, 4.0f, 2.0f, 1.0f});
 
     MatrixView alphaView = alpha.weightsView();
 
--- src/model/mlp.h
+++ src/model/mlp.h
@@ -1,17 +1,27 @@
 #pragma once
 
-#include <vector>
+#include <functional>
 #include "matrix.h"
+
+class Activation {
+    private:
+        std::function<float(float)> function;
+        std::function<float(float)> derivative;
+    
+    public:
+        Activation(std::function<float(float)> func, std::function<float(float)> der) 
+            : function(std::move(func)), derivative(std::move(der)) {};
+};
 
 class layer {
     private:
         MatrixOwner weights;
         MatrixOwner bias;
-        //MatrixOwner activation;
+        Activation activation;
 
     public:
-        //MatrixOwner forward(MatrixView x, MatrixOwner weights, MatrixOwner bias);
-        layer(size_t r, size_t c);
+        MatrixOwner forward(MatrixView& x);
+        layer(size_t r, size_t c, Activation act);
         void loadWeights(const std::vector<float>& vin) {weights.fill(vin);};
         void loadBias(const std::vector<float>& vin) {bias.fill(vin);};
         MatrixView weightsView() {return weights.view();};
